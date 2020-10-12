ad2f26c95b6f45cc25409f80187682e2a35bc633
BenNeighbour <benneighbour007@gmail.com>
2020-10-12 18:40:55 +0100

    Initial Commit

---
+++ api-common-library/src/test/java/com/footyandsweep/apicommonlibrary/ApiCommonLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footyandsweep.apicommonlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCommonLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ api-sweepstake-engine/src/main/java/com/footyandsweep/apisweepstakeengine/ApiSweepstakeEngineApplication.java
@@ -0,0 +1,12 @@
+package com.footyandsweep.apisweepstakeengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSweepstakeEngineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiSweepstakeEngineApplication.class, args);
+  }
+}
