6c6d71cf717d37621c64232705e3e02dbabd998e
BenNeighbour <benneighbour007@gmail.com>
2020-10-17 11:57:26 +0100

    Adding more kubernetes deployments

--- api-gateway-service/src/main/java/com/footyandsweep/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/footyandsweep/apigatewayservice/ApiGatewayServiceApplication.java
@@ -4,10 +4,12 @@ import com.footyandsweep.apigatewayservice.config.RedisConfiguration;
 import com.footyandsweep.apigatewayservice.config.WebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableJpaRepositories
 @Import({WebConfiguration.class, RedisConfiguration.class})
 public class ApiGatewayServiceApplication {
