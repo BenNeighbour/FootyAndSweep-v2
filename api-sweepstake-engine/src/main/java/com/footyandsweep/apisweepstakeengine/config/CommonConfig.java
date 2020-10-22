1e85d40e5e43a48a394039ac749de52ce62cf291
BenNeighbour <benneighbour007@gmail.com>
2020-10-22 07:00:49 +0100

    Adding apache license

--- api-sweepstake-engine/src/main/java/com/footyandsweep/apisweepstakeengine/config/CommonConfig.java
+++ api-sweepstake-engine/src/main/java/com/footyandsweep/apisweepstakeengine/config/CommonConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 FootyAndSweep
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.footyandsweep.apisweepstakeengine.config;
 
 import com.footyandsweep.apisweepstakeengine.dao.SweepstakeDao;
@@ -10,7 +26,6 @@ import io.eventuate.tram.events.subscriber.DomainEventDispatcherFactory;
 import io.eventuate.tram.spring.events.publisher.TramEventsPublisherConfiguration;
 import io.eventuate.tram.spring.events.subscriber.TramEventSubscriberConfiguration;
 import io.eventuate.tram.spring.jdbckafka.TramJdbcKafkaConfiguration;
-import io.eventuate.tram.spring.optimisticlocking.OptimisticLockingDecoratorConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,8 +37,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 @Import({TramJdbcKafkaConfiguration.class,
         TramEventsPublisherConfiguration.class,
-        TramEventSubscriberConfiguration.class,
-        OptimisticLockingDecoratorConfiguration.class})
+        TramEventSubscriberConfiguration.class})
 public class CommonConfig {
 
     @Bean
