17aae7bd7a5f5c98b639bfe0da4b8feba3042c05
BenNeighbour <benneighbour007@gmail.com>
2020-10-26 21:06:05 +0000

    Getting rid of commented out stuff

--- api-sweepstake-engine/src/main/java/com/footyandsweep/apisweepstakeengine/model/Sweepstake.java
+++ api-sweepstake-engine/src/main/java/com/footyandsweep/apisweepstakeengine/model/Sweepstake.java
@@ -17,15 +17,8 @@
 package com.footyandsweep.apisweepstakeengine.model;
 
 import com.footyandsweep.apicommonlibrary.model.SweepstakeCommon;
-import com.footyandsweep.apicommonlibrary.model.TicketCommon;
-import com.footyandsweep.apicommonlibrary.model.UserCommon;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "sweepstake")
