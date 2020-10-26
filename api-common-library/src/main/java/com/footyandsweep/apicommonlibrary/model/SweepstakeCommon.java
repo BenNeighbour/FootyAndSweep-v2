21070e7dce1c8e8a535039eb5224a87984aa8394
BenNeighbour <benneighbour007@gmail.com>
2020-10-26 21:02:37 +0000

    Fixing the entity problem

--- api-common-library/src/main/java/com/footyandsweep/apicommonlibrary/model/SweepstakeCommon.java
+++ api-common-library/src/main/java/com/footyandsweep/apicommonlibrary/model/SweepstakeCommon.java
@@ -35,9 +35,9 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @MappedSuperclass
-@Access(AccessType.PROPERTY)
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-public abstract class SweepstakeCommon implements Serializable {
+//@Access(AccessType.PROPERTY)
+//@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+public class SweepstakeCommon implements Serializable {
 
     private static final long serialVersionUID = -771315870335755392L;
 
