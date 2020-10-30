5ce408aceca65a8f654f90233e9dec777bcb4008
BenNeighbour <benneighbour007@gmail.com>
2020-10-30 14:00:22 +0000

    Adding kubernetes config stuff to the main classes

--- api-ticket-engine/src/main/java/com/footyandsweep/apiticketengine/ApiTicketEngineApplication.java
+++ api-ticket-engine/src/main/java/com/footyandsweep/apiticketengine/ApiTicketEngineApplication.java
@@ -2,12 +2,33 @@ package com.footyandsweep.apiticketengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiTicketEngineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTicketEngineApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public FilterRegistrationBean forwardHeadersFilterBean() {
+		FilterRegistrationBean bean = new FilterRegistrationBean(new ForwardedHeaderFilter());
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
