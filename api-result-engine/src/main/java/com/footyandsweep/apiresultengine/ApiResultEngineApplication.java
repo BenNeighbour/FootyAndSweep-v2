0e1c8dd5a195111293bced0530d28593fba1b42e
BenNeighbour <benneighbour007@gmail.com>
2020-10-30 13:01:20 +0000

    Adding all of the skaffold files the new services

---
+++ api-result-engine/src/main/java/com/footyandsweep/apiresultengine/ApiResultEngineApplication.java
@@ -0,0 +1,13 @@
+package com.footyandsweep.apiresultengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResultEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResultEngineApplication.class, args);
+	}
+
+}
