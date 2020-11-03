6e8261ab2b7e978e70dd78caaec7f19245c4fc5b
BenNeighbour <benneighbour007@gmail.com>
2020-11-03 20:25:49 +0000

    Adding more models to the sweepstake entity for football matches and types

---
+++ api-common-library/src/main/java/com/footyandsweep/apicommonlibrary/model/football/FootballMatchCommon.java
@@ -0,0 +1,51 @@
+/*
+ *   Copyright 2020 FootyAndSweep
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.footyandsweep.apicommonlibrary.model.football;
+
+import com.footyandsweep.apicommonlibrary.model.TeamCommon;
+import com.footyandsweep.apicommonlibrary.model.sweepstake.SweepstakeEventCommon;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Transient;
+import java.io.Serializable;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class FootballMatchCommon extends SweepstakeEventCommon implements Serializable {
+
+    private static final long serialVersionUID = -9169784048037914911L;
+
+    @Transient
+    private TeamCommon homeFootballTeam;
+
+    @Transient
+    private FootballMatchSquadCommon homeMatchSquad;
+
+    @Transient
+    private TeamCommon awayFootballTeam;
+
+    @Transient
+    private FootballMatchSquadCommon awayMatchSquad;
+
+}
