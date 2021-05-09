6cb44cbc5cd929fa3266678446854a9c3868cc8b
BenNeighbour <benneighbour007@gmail.com>
2021-05-09 21:07:25 +0100

    dev: Adding a few more things

--- ui-web-service/src/pages/Signup/SignupPage.tsx
+++ ui-web-service/src/pages/Signup/SignupPage.tsx
@@ -19,6 +19,7 @@ import React, {FunctionComponent, useEffect, useState} from 'react';
 import {useHistory} from "react-router-dom";
 import SignupForm from "../../views/SignupForm/SignupForm";
 import "./SignupPage.scss";
+import Logo from "../../icons/logo.png";
 
 interface OwnProps {
 }
@@ -42,6 +43,12 @@ const SignupPage: FunctionComponent<Props> = (props) => {
     return (
         <div className={`signupContainer`}>
             <div className={"mainSection"}>
+                <div className={"brandSection"}>
+                    <img src={Logo} alt={""} className={"smallLogo"}/>
+                    <div>
+                        <span className={"description"}>The best Sweepstakes Platform on the Planet.</span>
+                    </div>
+                </div>
                 <SignupForm history={history}
                             error={historyPreviousState !== undefined ? historyPreviousState.errors : null}/>
             </div>
